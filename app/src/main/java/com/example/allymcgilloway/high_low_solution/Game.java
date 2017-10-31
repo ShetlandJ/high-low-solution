18ea324f346de9a851777625b55693aa603fb90a
Scott Murray <rai.five@gmail.com>
2017-10-31 10:52:38 +0000

    Initial Commit

--- app/src/main/java/com/example/allymcgilloway/high_low_solution/Game.java
+++ app/src/main/java/com/example/allymcgilloway/high_low_solution/Game.java
@@ -1,14 +1,21 @@
 package com.example.allymcgilloway.high_low_solution;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import static android.media.CamcorderProfile.get;
+
+
 
 /**
  * Created by allymcgilloway on 17/10/2017.
  */
 
 public class Game {
+
+    Scanner scanner = new Scanner( System.in );
+
 
     private Deck deck;
     private Dealer dealer;
@@ -29,12 +36,36 @@ public class Game {
     }
 
     public void play() {
+        Participant player1 = players.get(0);
+        Participant player2 = players.get(1);
         dealer.startDealing(this.players, this.deck);
         ui.showDealtCards(this.players);
+        hitOrStay(player1);
+        System.out.println(player1.getHandValue());
+        hitOrStay(player2);
+        System.out.println(player2.getHandValue());
         Participant winner = checkWinner(players);
         ui.showWinner(winner);
 
     }
+    public void playerIsBust(Player player){
+        if (player.getHandValue() > 21) {
+            boolean gameOver = true;
+        }
+    }
+
+    public void hitOrStay(Participant player) {
+
+        System.out.println("Player, do you want to hit or stay?: ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        if (input == "H"){
+            dealer.dealCard(player, deck);
+        }
+    }
+
+
+
 
 
     public Participant checkWinner(ArrayList<Participant> players){
